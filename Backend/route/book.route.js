c08b130e5443eaca039fdedc1dfab8cd054d5b2e
Savage9193 <tyagishahvez551@gmail.com>
2024-09-13 05:26:55 +0530

    first

---
+++ Backend/route/book.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getBook } from "../controller/book.controller.js";
+
+import { get } from "mongoose";
+
+const router=express.Router();
+router.get("/",getBook);
+
+
+export default router;
