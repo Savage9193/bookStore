cdb06b196bedf857144fefa90c3e7188a099abe7
Savage9193 <tyagishahvez551@gmail.com>
2024-09-15 05:06:46 +0530

    update

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://book-store-1-seven.vercel.app//user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
