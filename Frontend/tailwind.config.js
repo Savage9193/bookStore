70e43ddac1b64c1cc36c62f033da8ef8437e8650
Savage9193 <tyagishahvez551@gmail.com>
2024-09-15 15:55:39 +0530

    update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,5 +1,33 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   darkMode: 'class',
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
+
+
+
+// module.exports = {
+//   theme: {
+//     screens: {
+//       sm: '640px',
+//       md: '768px',
+//       lg: '1024px',
+//       xl: '1280px',
+//     },
+//   },
+// }
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,6 +35,12 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [
     require('daisyui'),
